9e49441181e69fcca05535bbf5ce3300615551ac
pampanelson <pampa.nelson@gmail.com>
2015-06-25 19:20:53 +0800

    Initial Commit

---
+++ testWebView/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  testWebView
+//
+//  Created by pampa on 6/25/15.
+//  Copyright © 2015 pampa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController,UIWebViewDelegate {
+
+    
+    @IBOutlet weak var uiwv: UIWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let url = NSURL(string: "http://baidu.com")
+        let request = NSURLRequest(URL: url!)
+        
+        uiwv.loadRequest(request)
+        uiwv.delegate = self
+        
+    }
+
+    
+    func webViewDidFinishLoad(webView: UIWebView) {
+        //
+        print("did finished load web page")
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
